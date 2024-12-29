ad0790b8c1936f5097800a631ef8ef712f3b0049
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-12-29 23:34:25 +0000

    Fix the order of imports

--- models/__init__.py
+++ models/__init__.py
@@ -6,22 +6,21 @@ Attributes:
 """
 from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
-
 # Import models
-from models.user import User, user_roles  # Import user_roles from user.py
 from models.role import Role
+from models.user import User, user_roles
+from models.category import Category
+from models.tag import Tag
 from models.post import Post
+from models.post_tags import post_tags
+from models.comment import Comment
 from models.post_revision import PostRevision
 from models.post_like import PostLike
 from models.post_view import PostView
-from models.category import Category
-from models.tag import Tag, post_tags  # Import post_tags from tag.py
-from models.comment import Comment
 from models.audit_log import AuditLog
 from models.login_attempt import LoginAttempt
 from models.user_session import UserSession
-
-__all__ = [
+all = [
     "Base", "User", "Role", "Post", "PostRevision", "PostLike", "PostView",
     "Category", "Tag", "post_tags", "Comment", "AuditLog", "LoginAttempt",
     "UserSession", "user_roles"
